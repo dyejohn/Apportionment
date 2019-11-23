f4514b05361a3ebbbc84ea37f6055ef8da4d9d87
John Dye <geekydevguy@gmail.com>
2019-11-23 17:49:35 -0500

    Initial Commit

---
+++ Apportionment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Apportionment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateDetails stateDetails = new StateDetails();
+            stateDetails.Load();
+            stateDetails.SolveForMostRepresentation();
+            Console.WriteLine( stateDetails.RenderJustStatesAndSeats());
+        }
+    }
+}
---
+++ Apportionment/States.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apportionment
+{
+    public class StateDetails
+    {
+        List<State> _states = new List<State>();
+
+        //public void Load()
+        //{
+        //    // load all the states and get them set up.
+
+        //    using (System.IO.StreamReader sr = new System.IO.StreamReader("startingStateData.csv"))
+        //    {
+        //        var allStateData = sr.ReadToEnd();
+
+        //        string[] stateData = allStateData.Split('\n');
+
+        //        foreach(var item in stateData)
+        //        {
+        //            var deets = item.Split(',');
+        //            var values = item.Split("\"");
+
+        //            var state = new State()
+        //            {
+        //                Name = deets[0],
+        //                CensusPop = int.Parse(values[1].Replace(",","")),
+        //                CurrentPop = int.Parse(values[3].Replace(",", ""))
+
+        //            };
+        //            _states.Add(state);
+        //        }
+        //    }
+
+        //    var states = System.Text.Json.JsonSerializer.Serialize(_states);
+        //    System.IO.StreamWriter stw = new System.IO.StreamWriter("state_data.json");
+        //    stw.Write(states);
+        //    stw.Close();
+
+        //}
+
+        public void Load()
+        {
+
+            using (System.IO.StreamReader sr = new System.IO.StreamReader("state_data.json"))
+            {
+                var allStateData = sr.ReadToEnd();
+
+                _states = System.Text.Json.JsonSerializer.Deserialize<List<State>>(allStateData);
+                foreach(var state in _states)
+                {
+                    // everyone gets a seat at first.
+                    state.NumberOfSeats = 1;
+                }
+            }
+        }
+
+        public void SolveForMostRepresentation()
+        {
+            // we stop when there are 450!
+            while(_states.Sum(x => x.NumberOfSeats) < 450)
+            {
+                // find the state with the worst representation
+                _states.Sort(SortByReps);
+                _states.First().AddSeat();
+                
+            }
+        }
+
+        public string RenderResultsJson()
+        {
+            _states.Sort(SortBySeats);
+            return System.Text.Json.JsonSerializer.Serialize(_states);
+        }
+
+        public string RenderCSV()
+        {
+            _states.Sort(SortBySeats);
+            StringBuilder stb = new StringBuilder();
+            foreach(var state in _states)
+            {
+                stb.AppendLine($"{state.Name}, {state.CensusPop}, {state.CurrentPop}, {state.NumberOfSeats}, {state.PeoplePerSeat}");
+            }
+
+            return stb.ToString();
+        }
+
+        public string RenderJustStatesAndSeats()
+        {
+            _states.Sort(SortBySeats);
+            StringBuilder stb = new StringBuilder();
+            foreach (var state in _states)
+            {
+                stb.AppendLine($"{state.Name}, {state.NumberOfSeats}");
+            }
+
+            return stb.ToString();
+        }
+
+
+        // sort the least represented to the top. basically "descending"
+        private int SortByReps(State state1, State state2)
+        {
+            if(state1.PeoplePerSeat < state2.PeoplePerSeat)
+            {
+                return 1;
+            }
+
+            if(state1.PeoplePerSeat > state2.PeoplePerSeat)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private int SortBySeats(State state1, State state2)
+        {
+            if(state1.NumberOfSeats < state2.NumberOfSeats)
+            {
+                return 1;
+            }
+            if(state1.NumberOfSeats > state2.NumberOfSeats)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+
+    }
+}
